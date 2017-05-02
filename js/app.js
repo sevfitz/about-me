3a08732075de78f3608f4a190373b7c776d67edd
Stephanie Fitzgerald <s.e.v.fitzgerald@gmail.com>
2017-05-02 16:58:47 -0700

    Add more console messages

--- js/app.js
+++ js/app.js
@@ -7,8 +7,13 @@ function questionOne() {
 
 	if ( a1Upper === 'YES' || a1Upper === 'Y' ) {
 		a1.textContent = 'Yep. My hair is brown.';
+		a1.style.color = 'black';
+		console.log( 'The user entered a correct answer.' );
+
 	} else if ( a1Upper === 'NO' || a1Upper === 'N' ) {
 		a1.textContent = 'No. My hair is brown.';
+		a1.style.color = 'black';
+		console.log( 'The user entered an incorrect answer.' );
 	} else {
 		alert( 'Please enter a yes or no answer.' );
 		questionOne();
@@ -22,8 +27,12 @@ function questionTwo() {
 
 	if ( a2Upper === 'YES' || a2Upper === 'Y' ) {
 		a2.textContent = 'No. I\'m not nearly that tall.';
+		a2.style.color = 'black';
+        console.log( 'The user entered an incorrect answer.' );
 	} else if ( a2Upper === 'NO' || a2Upper === 'N' ) {
 		a2.textContent = 'You are correct. I am much shorter than that.';
+		a2.style.color = 'black';
+        console.log( 'The user entered a correct answer.' );
 	} else {
 		alert( 'Please enter a yes or no answer.' );
 		questionTwo();
@@ -37,8 +46,12 @@ function questionThree() {
 
 	if ( a3Upper === 'YES' || a3Upper === 'Y' ) {
 		a3.textContent = 'You are right--I did for a while.';
+		a3.style.color = 'black';
+        console.log( 'The user entered a correct answer.' );
 	} else if ( a3Upper === 'NO' || a3Upper === 'N' ) {
 		a3.textContent = 'Nope. I really did live there.';
+		a3.style.color = 'black';
+        console.log( 'The user entered an incorrect answer.' );
 	} else {
 		alert( 'Please enter a yes or no answer.' );
 		questionThree();
@@ -52,8 +65,12 @@ function questionFour() {
 
 	if ( a4Upper === 'YES' || a4Upper === 'Y' ) {
 		a4.textContent = 'You\'re right; I did study Physics for my Bachelor\'s.';
+		a4.style.color = 'black';
+        console.log( 'The user entered a correct answer.' );
 	} else if ( a4Upper === 'NO' || a4Upper === 'N' ) {
 		a4.textContent = 'No. I did study Physics.';
+		a4.style.color = 'black';
+        console.log( 'The user entered an incorrect answer.' );
 	} else {
 		alert( 'Please enter a yes or no answer.' );
 		questionFour();
@@ -67,8 +84,12 @@ function questionFive() {
 
 	if ( a5Upper === 'YES' || a5Upper === 'Y' ) {
 		a5.textContent = 'Nope, luckily!';
+		a5.style.color = 'black';
+        console.log( 'The user entered an incorrect answer.' );
 	} else if ( a5Upper === 'NO' || a5Upper === 'N' ) {
 		a5.textContent = 'You\'re right. I\'m not allergic to wheat.';
+		a5.style.color = 'black';
+        console.log( 'The user entered a correct answer.' );
 	} else {
 		alert( 'Please enter a yes or no answer.' );
 		questionFive();
