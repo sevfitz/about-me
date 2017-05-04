d7b0c09a21e8164fe462dc51a5bc216c3cf28d38
Stephanie Fitzgerald <s.e.v.fitzgerald@gmail.com>
2017-05-03 17:55:12 -0700

    Create arrays

--- js/app.js
+++ js/app.js
@@ -1,7 +1,26 @@
 'use strict';
 
 
+// TODO: Build question and answer arrays and use this loop to pull the questions and answers from them by index number
+// have one overall function to play the game using a button - no fxns inside the questions though
 
+var questionArray = ['1. Is Stephanie\'s hair brown?',
+					'2. Is Stephanie 5\'10\" tall?',
+					'3. Has Stephanie ever lived in France?',
+					'4. Did Stephanie study Physics?',
+					'5. Is Stephanie allergic to wheat?'];
+
+var answeredYesArray = ['Yep. My hair is brown.',
+					'No. I\'m not nearly that tall.',
+					'You are right--I did for a while.',
+					'You\'re right; I did study Physics for my Bachelor\'s.',
+					'Nope, luckily!'];
+					
+var answeredNoArray = ['No. My hair is brown.',
+					'You are correct. I am much shorter than that.',
+					'Nope. I really did live there.',
+					'No. I did study Physics.',
+					'You\'re right. I\'m not allergic to wheat.'];
 
 // Question One
 do {
