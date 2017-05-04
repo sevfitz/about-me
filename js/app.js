23ea126d3179dd433745ab7cd87c125080df67ff
ckrountree <jmail0207@gmail.com>
2017-05-04 14:26:56 -0700

    move all Q's into functions

--- js/app.js
+++ js/app.js
@@ -36,94 +36,109 @@ var userName = prompt( 'What is your name?' );
 var tally = 0;
 
 // Questions 1-5
-for ( var i = 0; i < questionArray.length - 2; i ++) {
-	do {
-		var answer = prompt( questionArray[i] ).toUpperCase();
-		var answerToWrite = document.getElementById( 'answer' + (i + 1) );
-		console.log( 'The response to Question ' + (i + 1) + ' was ' + answer );
 
-		if ( answer === 'YES' || answer === 'Y' ) {
-			answerToWrite.textContent = answeredYesArray[i];
-			answerToWrite.style.color = 'black';
-			if ( i === 0 || i === 2 || i === 3 ) {
-				answerToWrite.style.fontSize = '20px';
-				tally++;
-				console.log( rightAnswerMsg );
+function qOneToFive() {
+
+	for ( var i = 0; i < questionArray.length - 2; i ++) {
+		do {
+			var answer = prompt( questionArray[i] ).toUpperCase();
+			var answerToWrite = document.getElementById( 'answer' + (i + 1) );
+			console.log( 'The response to Question ' + (i + 1) + ' was ' + answer );
+
+			if ( answer === 'YES' || answer === 'Y' ) {
+				answerToWrite.textContent = answeredYesArray[i];
+				answerToWrite.style.color = 'black';
+				if ( i === 0 || i === 2 || i === 3 ) {
+					answerToWrite.style.fontSize = '20px';
+					tally++;
+					console.log( rightAnswerMsg );
+				} else {
+					console.log( wrongAnswerMsg );
+				}
+				break;
+
+			} else if ( answer === 'NO' || answer === 'N' ) {
+				answerToWrite.textContent = answeredNoArray[i];
+				answerToWrite.style.color = 'black';
+				if ( i === 1 || i === 4 ) {
+					answerToWrite.style.fontSize = '20px';
+					tally++;
+					console.log( rightAnswerMsg );
+				} else {
+					console.log( wrongAnswerMsg );
+				}
+				break;
+
 			} else {
-				console.log( wrongAnswerMsg );
+				alert( 'Please enter a yes or no answer.' );
 			}
-			break;
 
-		} else if ( answer === 'NO' || answer === 'N' ) {
-			answerToWrite.textContent = answeredNoArray[i];
-			answerToWrite.style.color = 'black';
-			if ( i === 1 || i === 4 ) {
-				answerToWrite.style.fontSize = '20px';
-				tally++;
-				console.log( rightAnswerMsg );
-			} else {
-				console.log( wrongAnswerMsg );
-			}
-			break;
-
-		} else {
-			alert( 'Please enter a yes or no answer.' );
-		}
-
-	} while ( answer !== '' );
+		} while ( answer !== '' );
+	}
 }
 
 // Question 6
-var compGuess = Math.floor(Math.random() * 10 + 1);
-var guessNum = 4;
 
-while ( guessNum > 0 ) {
-	var numAnswer = parseInt( prompt( questionArray[5] ) );
-	if ( isNaN( numAnswer ) ) {
-		alert( 'Please enter a number!' );
+function qSix() {
+	var compGuess = Math.floor(Math.random() * 10 + 1);
+	var guessNum = 4;
 
-	} else if ( numAnswer === compGuess ) {
-		alert( 'You got it! It took you '+ ( 5 - guessNum ) + ' guesses to get the right number!' );
-		tally++;
-		break;
+	while ( guessNum > 0 ) {
+		var numAnswer = parseInt( prompt( questionArray[5] ) );
+		if ( isNaN( numAnswer ) ) {
+			alert( 'Please enter a number!' );
 
-	} else if ( numAnswer < compGuess ) {
-		guessNum--;
-		alert( 'Too low! You have ' + ( guessNum ) + ' guesses left.' );
+		} else if ( numAnswer === compGuess ) {
+			alert( 'You got it! It took you '+ ( 5 - guessNum ) + ' guesses to get the right number!' );
+			tally++;
+			break;
 
-	} else if ( numAnswer > compGuess ) {
-		guessNum--;
-		alert( 'Too high! You have ' + ( guessNum ) + ' guesses left.' );
-//	} else {
-	//	alert( 'You got it! It took you ' ( guessNum ) + ' guesses to get the right number!' );
-		
+		} else if ( numAnswer < compGuess ) {
+			guessNum--;
+			alert( 'Too low! You have ' + ( guessNum ) + ' guesses left.' );
+
+		} else if ( numAnswer > compGuess ) {
+			guessNum--;
+			alert( 'Too high! You have ' + ( guessNum ) + ' guesses left.' );
+	//	} else {
+		//	alert( 'You got it! It took you ' ( guessNum ) + ' guesses to get the right number!' );
+			
+		}
 	}
-}
-var answerSix = document.getElementById( 'answer6' );
-answerSix.textContent = 'The number was ' + compGuess + '!';
-answerSix.style.color = 'black';
+	var answerSix = document.getElementById( 'answer6' );
+	answerSix.textContent = 'The number was ' + compGuess + '!';
+	answerSix.style.color = 'black';
 
+}
 
 // Question 7
-for ( var guessCount = 6; guessCount > 0; guessCount -- ) {
-	var cityAnswer = prompt( questionArray[6] ).toUpperCase();
-	var rightAnswer = false;
-	for ( var a = 0; a < cityArray.length; a ++ ) {
-		if ( cityAnswer === cityArray[a] ) {
-			rightAnswer = true;
-		} 
+function qSeven() {
+
+	for ( var guessCount = 6; guessCount > 0; guessCount -- ) {
+		var cityAnswer = prompt( questionArray[6] ).toUpperCase();
+		var rightAnswer = false;
+		for ( var a = 0; a < cityArray.length; a ++ ) {
+			if ( cityAnswer === cityArray[a] ) {
+				rightAnswer = true;
+			} 
+		}
+		if ( rightAnswer === true ) {
+			alert( 'You\'re right!' );
+			tally++;
+			guessCount = 0;
+			break;
+		} else {
+			alert( 'No.' );
+		}
 	}
-	if ( rightAnswer === true ) {
-		alert( 'You\'re right!' );
-		tally++;
-		guessCount = 0;
-		break;
-	} else {
-		alert( 'No.' );
-	}
+	var answerSeven = document.getElementById( 'answer7' );
+	answerSeven.textContent = 'Here are all the correct answers: ' + cityArray;
+	answerSeven.style.color = 'black';
+
 }
-var answerSeven = document.getElementById( 'answer7' );
-answerSeven.textContent = 'Here are all the correct answers: ' + cityArray;
-answerSeven.style.color = 'black';
+
+qOneToFive();
+qSix();
+qSeven();
 
 alert( userName + ', you got ' + tally + ' out of 7 questions right. Try again sometime!' );
